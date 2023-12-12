1ec2427d1b908938c5542bf91e3c5daa23d664ec
dacastro42 <alejandro6544@gmail.com>
2023-12-12 18:10:08 -0500

    mod
    Se modifica el insert de partido.. por ahora falta el insert... solo esta el mostrar los equipos para selección de local y visitante

--- futbolv2ej/views/InsertPartido.cs
+++ futbolv2ej/views/InsertPartido.cs
@@ -14,6 +14,7 @@ namespace futbolv2ej.views
 {
     public partial class InsertPartido : Form
     {
+        List<Equipo> listD;
         public InsertPartido()
         {
             InitializeComponent();
@@ -22,12 +23,35 @@ namespace futbolv2ej.views
         private void LoadEquipos(object sender, EventArgs e)
         {
             ControllerPartido objc = new ControllerPartido();
-            List<Equipo> listD = objc.ConsultarEquipos();
+            listD = objc.ConsultarEquipos();
             for (int i = 0; i < listD.Count; i++)
             {
                 comboBox1.Items.Add(listD[i].NombreE);
                 comboBox2.Items.Add(listD[i].NombreE);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selected1 = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+            string selected2 = this.comboBox1.GetItemText(this.comboBox2.SelectedItem);
+            int idLocal=0;
+            int idVisitante= 0;
+            for (int i = 0; i < listD.Count; i++)
+            {
+                if (selected1.Equals(listD[i].NombreE))
+                {
+                    idLocal = listD[i].Ideqiupo;
+                }
+                if (selected2.Equals(listD[i].NombreE))
+                {
+                    idVisitante = listD[i].Ideqiupo;
+                }
+            }
+
+            //Realizar el insert en partido
+
+            //MessageBox.Show("ID Local "+ idLocal + " ID Visitante "+ idVisitante);
         }
     }
 }
